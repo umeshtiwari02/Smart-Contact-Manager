79c5776b4bf01a2ea8f7d8878cf62ce5d099d590
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-04 13:54:26 +0530

    base page added

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.smart.dao.UserRepository;
@@ -13,8 +14,9 @@ public class HomeController {
 	private UserRepository userRepository;
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		
+		model.addAttribute("title", "Home - Smart Contact Manager");
 		return "home";
 	}
 	

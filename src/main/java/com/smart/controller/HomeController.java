13bda824c08ce2e36b2bee7f912943832e8a594a
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-18 19:09:11 +0530

    server side validation on registration

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.smart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.smart.entities.User;
 import com.smart.helper.Messages;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -50,13 +52,19 @@ public class HomeController {
 	// Handler for register user
 
 	@PostMapping("/do_register")
-	public String registerUser(@ModelAttribute User user, @RequestParam(defaultValue = "false") boolean agreement,
-			Model model, HttpSession session) {
+	public String registerUser(@Valid @ModelAttribute User user, BindingResult result,
+			@RequestParam(defaultValue = "false") boolean agreement, Model model, HttpSession session) {
 
 		try {
 			if (!agreement) {
 				System.out.println("You have not agreed the terms and conditions.");
 				throw new Exception("You have not agreed the terms and conditions.");
+			}
+
+			if (result.hasErrors()) {
+				System.out.println("ERROR " + result.toString());
+				model.addAttribute("user", user);
+				return "signup";
 			}
 
 			user.setRole("ROLE_USER");
--- src/main/java/com/smart/entities/User.java
+++ src/main/java/com/smart/entities/User.java
@@ -12,6 +12,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
@@ -21,6 +23,8 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
+	@NotBlank(message = "Name field is required !!")
+	@Size(min = 3, max = 25, message = "Min 3 and Max 25 characters are allowed !!")
 	private String name;
 
 	@Column(unique = true)

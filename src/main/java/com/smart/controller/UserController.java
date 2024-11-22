8eef2eede86e0df7c6f3983cc547897583977bcd
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-23 01:53:56 +0530

    process contact form data into database

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -53,6 +56,25 @@ public class UserController {
 		model.addAttribute("title", "Add Contact");
 		model.addAttribute("contact", new Contact());
 
+		return "normal/add_contact_form";
+	}
+
+	// processing add contact form
+	@PostMapping("/process-contact")
+	public String processContact(@ModelAttribute Contact contact, Principal principal) {
+
+		String name = principal.getName();
+
+		User user = this.userRepository.getUserByUserName(name);
+
+		contact.setUser(user);
+		user.getContacts().add(contact);
+
+		this.userRepository.save(user);
+
+		System.out.println("Data : " + contact);
+
+		System.out.println("Added to database");
 		return "normal/add_contact_form";
 	}
 

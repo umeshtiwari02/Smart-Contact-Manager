cb09ec7c8582df40858eb40d0619d28df5ce049f
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-22 15:08:23 +0530

    responsive sidebar is updated

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,16 @@
 console.log("This is script file")
+
+const toggleSidebar = () => {
+	console.log("Clicked")
+	if ($(".sidebar").is(":visible")) {
+		// if true -- need to close
+
+		$(".sidebar").css("display", "none");
+		$(".content").css("margin-left", "0%");
+	} else {
+		//if false -- need to show
+
+		$(".sidebar").css("display", "block");
+		$(".content").css("margin-left", "20%");
+	}
+};

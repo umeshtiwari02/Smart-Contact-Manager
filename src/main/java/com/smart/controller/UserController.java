142ec7b042327c32583db7db2f03e31538678f0a
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-23 13:49:08 +0530

    upload image into folder

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,20 +1,26 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
-
-import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -61,20 +67,46 @@ public class UserController {
 
 	// processing add contact form
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact, Principal principal) {
+	public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
+			Principal principal) {
 
-		String name = principal.getName();
+		try {
+			String name = principal.getName();
 
-		User user = this.userRepository.getUserByUserName(name);
+			User user = this.userRepository.getUserByUserName(name);
 
-		contact.setUser(user);
-		user.getContacts().add(contact);
+			// processing and uploading file
+			if (file.isEmpty()) {
+				System.out.println("File is empty!!");
+			} else {
 
-		this.userRepository.save(user);
+				// upload file into the folder and update the file name to the contact
 
-		System.out.println("Data : " + contact);
+				contact.setImage(file.getOriginalFilename());
 
-		System.out.println("Added to database");
+				File saveFile = new ClassPathResource("/static/img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				System.out.println("Image is uploaded!!");
+			}
+
+			user.getContacts().add(contact);
+			contact.setUser(user);
+
+			this.userRepository.save(user);
+
+			System.out.println("Data : " + contact);
+
+			System.out.println("Added to database");
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
 		return "normal/add_contact_form";
 	}
 

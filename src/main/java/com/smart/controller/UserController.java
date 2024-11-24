163e31f5566dc8526af1ef2c951597a82c32e69d
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-25 02:54:54 +0530

    showing all contacts in view contacts

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -31,6 +33,9 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private ContactRepository contactRepository;
 
 	// methods for adding common data to response
 
@@ -119,6 +124,22 @@ public class UserController {
 
 			return "normal/add_contact_form";
 		}
+	}
+
+	// show contact handler
+	@GetMapping("/show-contacts")
+	public String showContact(Model model, Principal principal) {
+
+		String userName = principal.getName();
+
+		User user = this.userRepository.getUserByUserName(userName);
+
+		List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+
+		model.addAttribute("contacts", contacts);
+
+		model.addAttribute("title", "Show User Contacts");
+		return "normal/show_contacts";
 	}
 
 }

62322b3e0d3c087b915d02690d10282af3b6515b
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-11-29 16:46:57 +0530

    feat: add and implement search functionality

--- src/main/java/com/smart/dao/ContactRepository.java
+++ src/main/java/com/smart/dao/ContactRepository.java
@@ -1,5 +1,7 @@
 package com.smart.dao;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.Contact;
+import com.smart.entities.User;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
@@ -20,5 +23,8 @@ public interface ContactRepository extends JpaRepository<Contact, Integer> {
 	@Modifying
 	@Query("DELETE FROM Contact c WHERE c.cid = :cid")
 	void deleteContactById(@Param("cid") int cid);
+	
+	// searching
+	public List<Contact> findByNameContainingAndUser(String name, User user);
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -48,3 +48,40 @@ function deleteUser(id) {
 			}
 		});
 };
+
+const search = () => {
+
+	let query = $("#search-input").val();
+
+	if (query == '') {
+		$(".search-result").hide();
+
+	} else {
+		// searching logic
+		// console.log(query);
+
+		// sending request to server
+		let url = `http://localhost:8080/search/${query}`;
+
+		fetch(url).then((response) => {
+
+			return response.json();
+
+		}).then((data) => {
+			// data
+			// console.log(data);
+
+			let text = `<div class='list-group'>`;
+
+			data.forEach((contact) => {
+				text += `<a href='/user/${contact.cid}/contact' class='list-group-item list-group-item-action'> ${contact.name} </a>`;
+			});
+
+			text += `</div>`;
+
+			$(".search-result").html(text);
+			$(".search-result").show();
+
+		});
+	}
+}

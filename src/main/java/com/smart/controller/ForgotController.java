2a3de8a319cc33630bb2e0f59bb2c1b69ac14ed1
Umesh Tiwari <klau.tiwari1234@gmail.com>
2024-12-11 14:44:26 +0530

    Implement OTP verification and email validation for password forgot/reset process.

--- src/main/java/com/smart/controller/ForgotController.java
+++ src/main/java/com/smart/controller/ForgotController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 import com.smart.services.EmailService;
 
 import jakarta.servlet.http.HttpSession;
@@ -16,6 +18,9 @@ import jakarta.servlet.http.HttpSession;
 public class ForgotController {
 
 	Random random = new Random(10000);
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Autowired
 	private EmailService emailService;
@@ -39,24 +44,50 @@ public class ForgotController {
 		// code for sending OTP to your email
 
 		String subject = "OTP from Smart Contact Manager";
-		String message = ""
-		+ "<div>"
-		+ "<h2>"
-		+ "Your OTP is "
-		+ otp
-		+ "</h2>"
-		+ "</div>";
+		String message = "" + "<div>" + "<h2>" + "Your OTP is " + otp + "</h2>" + "</div>";
 		String to = email;
 
 		boolean flag = this.emailService.sendEmail(subject, message, to);
 
 		if (flag) {
 
+			session.setAttribute("myotp", otp);
+			session.setAttribute("email", email);
 			return "verify_otp";
 		} else {
 			session.setAttribute("message", "Please check your email id!!");
 
 			return "forgot_email_form";
+		}
+
+	}
+
+	// verifying OTP
+	@PostMapping("/verify-otp")
+	public String verifyOtp(@RequestParam int otp, HttpSession session) {
+
+		int myOtp = (int) session.getAttribute("myotp");
+		String email = (String) session.getAttribute("email");
+
+		if (myOtp == otp) {
+
+			// password change form
+			User user = this.userRepository.getUserByUserName(email);
+
+			if (user == null) {
+				// send error message
+				session.setAttribute("message", "User does not exist with this email!!");
+
+				return "forgot_email_form";
+			} else {
+				// send to change password form
+				return "password_change_form";
+			}
+
+		} else {
+
+			session.setAttribute("message", "You have entered wrong OTP!!");
+			return "verify_otp";
 		}
 
 	}
